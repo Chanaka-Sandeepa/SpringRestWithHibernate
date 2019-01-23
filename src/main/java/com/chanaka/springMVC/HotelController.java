886ef45bd8239cd9dcf352545726b1786d9c038d
chanaka-sandeepa <chanaka.14@cse.mrt.ac.lk>
2019-01-23 11:54:43 +0530

    hotel add/update features added

--- src/main/java/com/chanaka/springMVC/HotelController.java
+++ src/main/java/com/chanaka/springMVC/HotelController.java
@@ -32,19 +32,15 @@ public class HotelController {
 
     //For add and update hotel both
     @RequestMapping(value= "/hotel/add", method = RequestMethod.POST)
-    public List<Hotel> addHotel(@ModelAttribute("hotel") Hotel h, @RequestParam("name") String name,
+    public List<Hotel> addHotel(@ModelAttribute("hotel") Hotel h, @RequestParam(value = "id", required = false) String id, @RequestParam("name") String name,
                            @RequestParam("address") String address, @RequestParam("city") String City,@RequestParam("country") String Country){
+        if (id != null){
+            this.hotelService.updateHotel(h);
+        }else {
+            this.hotelService.addHotel(h);
+        }
 
-//        if(h.getId() == 0){
-//            //new hotel, add it
-//            this.hotelService.addHotel(h);
-//        }else{
-//            //existing hotel, call update
-//            this.hotelService.updateHotel(h);
-//        }
-        this.hotelService.addHotel(h);
         return this.hotelService.listHotels();
-
     }
 
     @RequestMapping("/remove/{id}")
